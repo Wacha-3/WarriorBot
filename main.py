afa4dbeebe0f71fda9e54fd9dcae11d4f50202b0
Wacha <Robcartaco7@gmail.com>
2021-08-19 20:27:42 -0400

    first stem into oop

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-enemyName = input('What is the name of the enemy: ')
-maxHealth = int(input('What is the health of the enemy: '))
-health = maxHealth
-
-while health >= 0:
-    health = health - int(input('Damage: '))
-    print(str(health)+('/')+str(maxHealth))
-
-print(enemyName + ' has been defeated good work warrior!!')
-
-print('the next enemy will be: goblin')
+class Enemy:
+    def introduce_self(self):
+        print(self.name)
 
 
-#add individial score tracking per monster girl
+
+players=4
+e=Enemy
+e.name = input('What is the name of the enemy: ')
+players = int(input('How many players: '))
+e.health=75*players
+maxHealth = e.health
+
+print('Prepare to face ' + e.name + ': ('+str(maxHealth)+'/'+ str(maxHealth)+')')
+
+while e.health >= 1:
+    e.health = e.health - int(input('Damage: '))
+    print('HP: ('+str(e.health)+('/')+str(maxHealth)+')')
+
+print(e.name + ' has been defeated good work warrior!!')
+
+print('the next enemy will be: ') #+ e.name)
+
+
+#add individial score tracking per monster 
 
 #add oop enemy classes
 
