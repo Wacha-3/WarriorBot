49d57258209b632de5243438584a4be9f8ae0fc5
Wachamacolet <46825266+Wacha-3@users.noreply.github.com>
2021-08-21 16:57:01 +0000

    actual discord integration

--- main.py
+++ main.py
@@ -1,31 +1,67 @@
 import discord
+import os
+import datetime
+
+today = datetime.datetime.now()
+client = discord.Client()
 
 class Enemy:
+
+    name = 'minion'
+    HealthPerPlayer = 75
+    maxRep = 20
+    maxHealth = 0
+    currentHealth = 0
+
     def introduce_self(self):
         print(self.name)
 
 
+class Player:
 
-players=4
-e=Enemy
-e.name = input('What is the name of the enemy: ')
-players = int(input('How many players: '))
-e.health=75*players
-maxHealth = e.health
-
-print('Prepare to face ' + e.name + ': ('+str(maxHealth)+'/'+ str(maxHealth)+')')
-
-while e.health >= 1:
-    e.health = e.health - int(input('Damage: ')) 
-    print('HP: ('+str(e.health)+('/')+str(maxHealth)+')')
-
-print(e.name + ' has been defeated good work warrior!!')
-
-print('the next enemy will be: ') #+ e.name)
+    name = 'rob'
+    damageDealt = 0
+   
+   
+    def introduce_self(self):
+        print(self.name)
 
 
-#add individial score tracking per monster 
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
-#add oop enemy classes
 
-#set a schedule for what monsters will apear
+#Method to check if something is an int
+def RepresentsInt(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+#When a message is sent
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+
+#checks if $d was used and then adds damage to the monster and the players total
+    if message.content.startswith('$d') or message.content.startswith('$damage') :
+        dmg = message.content.replace("$d", "")
+        #checks if it is an int then if it is prints the values
+        if RepresentsInt(dmg) and int(dmg) >= 0 and int(dmg) <= 10:
+            dmg = message.author.name + "," + dmg + "," + today.strftime(' %b-%d')
+
+
+           
+    elif message.content.startswith('$p'):
+        msg = message.content.replace("$p", "")
+
+    else:
+        await message.channel.send('Invalid Syntax ' + message.author.name)
+
+client.run(os.getenv('TOKEN'))
+
