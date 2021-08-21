3c96e5a9a9a0c078eb016bd501fd34b02bbd4dbb
Wachamacolet <46825266+Wacha-3@users.noreply.github.com>
2021-08-21 18:54:14 +0000

    User Joining and damage

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 
 today = datetime.datetime.now()
 client = discord.Client()
-
+playerList = []  #initialise the array
 class Enemy:
 
     name = 'minion'
@@ -18,10 +18,10 @@ class Enemy:
 
 
 class Player:
-
-    name = 'rob'
-    damageDealt = 0
-   
+    def __init__(self, name): 
+       self.name = name
+       self.damageDealt = 0
+       
    
     def introduce_self(self):
         print(self.name)
@@ -29,7 +29,7 @@ class Player:
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('logged in as {0.user}'.format(client))
 
 
 #Method to check if something is an int
@@ -47,21 +47,30 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+#Takes the name of players and adds it into a list
+    if message.content.startswith('$join'):
+        playerList.append(Player(message.author.name)) #append the array and create a Player object
+        await message.channel.send(playerList[-1].name + ' has joined the arena!') #sends the name 
+
+        for obj in playerList:
+          print(obj.name )
+        
+
 
 #checks if $d was used and then adds damage to the monster and the players total
-    if message.content.startswith('$d') or message.content.startswith('$damage') :
+    if message.content.startswith('$d') or message.content.startswith('$damage'):
         dmg = message.content.replace("$d", "")
         #checks if it is an int then if it is prints the values
-        if RepresentsInt(dmg) and int(dmg) >= 0 and int(dmg) <= 10:
-            dmg = message.author.name + "," + dmg + "," + today.strftime(' %b-%d')
-
+        if RepresentsInt(dmg) and int(dmg) >= 0 and int(dmg) <= 10:           
+            playerList[0].damageDealt =  playerList[0].damageDealt + int(dmg) 
+            await message.channel.send(playerList[0].name + ' has dealt'+ dmg +' damage!')
 
            
-    elif message.content.startswith('$p'):
-        msg = message.content.replace("$p", "")
+  
 
-    else:
-        await message.channel.send('Invalid Syntax ' + message.author.name)
+
+    #elif:
+     #   await message.channel.send('Invalid Syntax ' + message.author.name)
 
 client.run(os.getenv('TOKEN'))
 
