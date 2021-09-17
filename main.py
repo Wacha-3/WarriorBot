90771d4f1e1e7c1d14978856a71a162d998d050d
Wachamacolet <46825266+Wacha-3@users.noreply.github.com>
2021-09-17 15:58:19 +0000

    Rewrote the thing with @bot.command now

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+
 import discord
+import tinydb
 import os
 import datetime
 
+from discord.ext import commands
+
 today = datetime.datetime.now()
-client = discord.Client()
+
 playerList = []  #initialise the array
 currentPlayers = []
+bot = commands.Bot(command_prefix='$')
 
-
+#initiallise an array of minions, and comapre it to date time to decide what enemies will apear 
 class Enemy:
     def __init__(self, name, maxHealth): 
        self.name = name
@@ -29,124 +34,93 @@ class Player:
         print(self.name)
 
 
-@client.event
-async def on_ready():
-    print('logged in as {0.user}'.format(client))
-
-
-
 #Method to check if something is an int
-def RepresentsInt(s):
+def IsInt(s):
     try:
         int(s)
         return True
     except ValueError:
         return False
 
+@bot.event
+async def on_ready():
+    print('logged in as {0.user}'.format(bot))
 
 
-#When a message is sent
-@client.event
-async def on_message(message):
-  global gameRunning 
-  global UserNumThatSentMsg 
-  global totalDmgSum
-  for x in range(len(currentPlayers)):  
-        if message.author.name == currentPlayers[x]:
+@bot.command(name='join')
+async def join(ctx):
+  author = str(ctx.author)[:-5] 
+  if currentPlayers.count(author) == 0:
+
+          playerList.append(Player(author)) #append the array and create a Player object
+          currentPlayers.append(author) 
+          await ctx.send(author + ' has joined the arena!') #sends the name 
+          
+          global enemy
+          if len(currentPlayers) == 1:
+            global hpScale
+            hpScale = 75
+            enemy = Enemy('Crab',len(playerList)*hpScale) #change this multiplier to something scalable for each\
+            await ctx.send('A ' +enemy.name + ' has apeared in the arena! :crossed_swords:')
+            
+            enemy.health = enemy.maxHealth
+
+          elif(enemy.alive):
+            enemy.health = enemy.health + hpScale
+            enemy.maxHealth = enemy.maxHealth + hpScale
+            await ctx.send(enemy.name + ' has grown in strength '+ str(enemy.health) + '/' + str(enemy.maxHealth))
+              
+  else:  
+        await ctx.send(author + ' has already joined the arena!') #sends the name 
+
+
+@bot.command(name='d')
+async def damage(ctx, arg):
+  if IsInt(arg) and int(arg) >= 0 and int(arg) <= 200:
+   author = str(ctx.author)[:-5] 
+   for x in range(len(currentPlayers)):  
+        if author == currentPlayers[x]:
           UserNumThatSentMsg = x
           break
-        elif message.author.name == currentPlayers[0]:
+        elif author == currentPlayers[0]:
           UserNumThatSentMsg = 0
           break
-  if message.author == client.user:
-      return
-      
 
-      
-     
-
-  elif message.content.startswith('$start'):     
-       gameRunning = 1
-  elif message.content.startswith('$start'):  
-       gameRunning = 0
-  elif message.content.startswith('$score'):
-       for x in range(len(playerList)):
-        await message.channel.send(playerList[x].name + ' has dealt '+ str(playerList[x].damageDealt) +' damage in total!')
-
-
+  playerList[UserNumThatSentMsg].damageDealt =  playerList[UserNumThatSentMsg].damageDealt + int(arg)    
+  enemy.health = enemy.health - int(arg)
+  await ctx.send(author + ' has dealt '+ arg +' damage!')
+  await ctx.send(str(enemy.health) + '/' + str(enemy.maxHealth))
+ 
+  if (enemy.health <= 0 and enemy.alive == 1):
+    await ctx.send(enemy.name + ' has been defeated! Great job warriors :crossed_swords:')
+    gameRunning = 0
+    enemy.alive = 0
+    for x in range(len(playerList)):
+      await ctx.send(playerList[x].name + ' has dealt '+ str(playerList[x].damageDealt) +' damage in total!')     
+      await ctx.send('The next enemy will be the boss monster "GIANT CRAB"')
+        
 
 
+@bot.command(name='hp')
+async def hp(ctx):
+  await ctx.send(str(enemy.health) + '/' + str(enemy.maxHealth))
 
 
-        #Takes the name of players and adds it into a list
-  elif message.content.startswith('$join'):    
 
-    if currentPlayers.count(message.author.name) == 0:
+@bot.command(name='score')
+async def score(ctx):
+  for x in range(len(playerList)):
+   await ctx.send(playerList[x].name + ' has dealt '+ str(playerList[x].damageDealt) +' damage in total!')
 
-         playerList.append(Player(message.author.name)) #append the array and create a Player object
-         currentPlayers.append(message.author.name) 
-         await message.channel.send(message.author.name + ' has joined the arena!') #sends the name 
-         
-         global enemy
-         if len(currentPlayers) == 1:
-          global hpScale
-          hpScale = 75
-          enemy = Enemy('Crab',len(playerList)*hpScale) #change this multiplier to something scalable for each\
-          await message.channel.send('A ' +enemy.name + ' has apeared in the arena! :crossed_swords:')
-          
-          enemy.health = enemy.maxHealth
-
-         elif(enemy.alive):
-          enemy.health = enemy.health + hpScale
-          enemy.maxHealth = enemy.maxHealth + hpScale
-          await message.channel.send(enemy.name + ' has grown in strength '+ str(enemy.health) + '/' + str(enemy.maxHealth))
-            
-    else:  
-         await message.channel.send(message.author.name + ' has already joined the arena!') #sends the name 
+@bot.command(name='bg')
+async def bg(ctx):
+  await ctx.send(playerList[-1].name)
+ # await ctx.send(playerList[UserNumThatSentMsg].name)
+  await ctx.send(len(playerList))    
 
 
 
 
 
 
-        #checks if $d was used and then adds damage to the monster and the players total
-  elif message.content.startswith('$d'):
-       dmg = message.content.replace("$d", "")
-                    #checks if it is an int then if it is prints the values
-       if RepresentsInt(dmg) and int(dmg) >= 0 and int(dmg) <= 100:           
-          playerList[UserNumThatSentMsg].damageDealt =  playerList[UserNumThatSentMsg].damageDealt + int(dmg)    
-          enemy.health = enemy.health - int(dmg)
-          await message.channel.send(message.author.name + ' has dealt'+ dmg +' damage!')
-          await message.channel.send(str(enemy.health) + '/' + str(enemy.maxHealth))
-          if (enemy.health <= 0 and enemy.alive == 1):
-            await message.channel.send(enemy.name + ' has been defeated! Great job warriors :crossed_swords:')
-            gameRunning = 0
-            enemy.alive = 0
-            for x in range(len(playerList)):
-              await message.channel.send(playerList[x].name + ' has dealt '+ str(playerList[x].damageDealt) +' damage in total!')     
-            await message.channel.send('The next enemy will be the boss monster "GIANT CRAB"')
-            
-
-
-
-  elif message.content.startswith('$hp'):
-       await message.channel.send(str(enemy.health) + '/' + str(enemy.maxHealth))
-  
-
-
-
-  elif message.content.startswith('$bg'):
-       await message.channel.send(playerList[-1].name)
-       await message.channel.send(playerList[UserNumThatSentMsg].name)
-       await message.channel.send(len(playerList))    
-#  elif message.content.startswith('$'):
-   #  await message.channel.send('Invalid Syntax ' + message.author.name)
-
-
-
-   
-  
-  
-
-client.run(os.getenv('TOKEN'))
-
+bot.run(os.getenv("TOKEN"))
