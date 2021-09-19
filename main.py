bb1106bab250a254b294e1a2573b56eff65aff69
Wachamacolet <46825266+Wacha-3@users.noreply.github.com>
2021-09-19 22:31:45 +0000

    actually kinda functional

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
-import discord
-import tinydb
-import os
+import discord  #imports discord library
+import tinydb   #import tinydb library (database)
+import os       #working with operating system
 import datetime
 
 from tinydb import TinyDB, Query
@@ -11,9 +10,12 @@ today = datetime.datetime.now()
 
 playerList = []  #initialise the array
 currentPlayers = []
+excercise = []
 bot = commands.Bot(command_prefix='$')
 db = TinyDB('db.json')
 User = Query()
+
+
 #initiallise an array of minions, and comapre it to date time to decide what enemies will apear 
 class Enemy:
     def __init__(self, name, maxHealth): 
@@ -21,9 +23,17 @@ class Enemy:
        self.maxHealth = maxHealth
        self.health = 0
        self.alive = 1
+       self.exercise = ""
 
     def introduce_self(self):
         print(self.name)
+
+class Excercise:
+    def __init__(self, name, minRep, maxRep, hpScale):
+      self.name = name
+      self.minRep = minRep #make a list of excerices then use the perams to scale enemy
+      self.maxRep = maxRep
+      self.hpScale = hpScale
 
 
 class Player:
@@ -35,7 +45,7 @@ class Player:
     def introduce_self(self):
         print(self.name)
 
-
+""""
 #Method to check if something is an int
 def IsInt(s):
     try:
@@ -43,14 +53,54 @@ def IsInt(s):
         return True
     except ValueError:
         return False
-
+"""
 @bot.event
 async def on_ready():
     print('logged in as {0.user}'.format(bot))
+   
+
+@bot.command(name='ex')
+async def ex(ctx, arg): #yooooooooooooooooo echooooooo
+  author = str(ctx.author)[:-5] 
+  global ExNum 
+  excercise.append(Excercise("Traps",30,60,150))
+  excercise.append(Excercise("Biceps",8,15,75))
+  excercise.append(Excercise("Shoulders",15,30,100))
+  excercise.append(Excercise("Calves",30,60,150))
+  excercise.append(Excercise("Core",0,500,250))
+
+  excercise.append(Excercise("Back",0,20,100))
+  excercise.append(Excercise("Chest",0,30,100))
+  excercise.append(Excercise("Triceps",0,35,100))
+  excercise.append(Excercise("Thighs",0,35,200))
+
+  if arg == "Traps"  or arg == "traps" or arg == "1":
+    ExNum = 0
+  elif arg == "Biceps" or arg == "biceps" or arg == "2" :
+    ExNum = 1
+  elif arg == "Shoulders" or arg == "shoulders" or arg == "3" :
+    ExNum = 2
+  elif arg == "Calves" or arg == "calves" or arg == "4" :
+    ExNum = 3
+  elif arg == "Core" or arg == "core" or arg == "5" :
+    ExNum = 4 
+  elif arg == "Back" or arg == "back" or arg == "6" :
+    ExNum = 5
+  elif arg == "Chest" or arg == "chest" or arg == "7" :
+    ExNum = 6
+  elif arg == "Triceps" or arg == "triceps" or arg == "8" :
+    ExNum = 7
+  elif arg == "Thighs" or arg == "thighs" or arg == "9" :
+    ExNum = 8
+  else:
+    await ctx.send(author + " has entered an invalid Excercise")
+  await ctx.send(author + " has said " + arg)
+  await ctx.send(author + " has set the excercise to " + excercise[ExNum].name)
 
 
 @bot.command(name='join')
 async def join(ctx):
+  global ExNum 
   author = str(ctx.author)[:-5] 
   if currentPlayers.count(author) == 0:
 
@@ -60,16 +110,14 @@ async def join(ctx):
           
           global enemy
           if len(currentPlayers) == 1:
-            global hpScale
-            hpScale = 75
-            enemy = Enemy('Crab',len(playerList)*hpScale) #change this multiplier to something scalable for each\
+            enemy = Enemy('Crab',len(playerList)*excercise[ExNum].hpScale) #change this multiplier to something scalable for each\
             await ctx.send('A ' +enemy.name + ' has apeared in the arena! :crossed_swords:')
             
             enemy.health = enemy.maxHealth
 
           elif(enemy.alive):
-            enemy.health = enemy.health + hpScale
-            enemy.maxHealth = enemy.maxHealth + hpScale
+            enemy.health = enemy.health + excercise[ExNum].hpScale
+            enemy.maxHealth = enemy.maxHealth + excercise[ExNum].hpScale
             await ctx.send(enemy.name + ' has grown in strength '+ str(enemy.health) + '/' + str(enemy.maxHealth))
               
   else:  
@@ -78,8 +126,9 @@ async def join(ctx):
 
 @bot.command(name='d')
 async def damage(ctx, arg):
-  if IsInt(arg) and int(arg) >= 0 and int(arg) <= 200:
-   author = str(ctx.author)[:-5] 
+  global ExNum
+  author = str(ctx.author)[:-5] 
+  if int(arg) >= excercise[ExNum].minRep and int(arg) <= excercise[ExNum].maxRep:
    for x in range(len(currentPlayers)):  
         if author == currentPlayers[x]:
           UserNumThatSentMsg = x
@@ -88,23 +137,27 @@ async def damage(ctx, arg):
           UserNumThatSentMsg = 0
           break
 
-  playerList[UserNumThatSentMsg].damageDealt =  playerList[UserNumThatSentMsg].damageDealt + int(arg)    
-  enemy.health = enemy.health - int(arg)
-  await ctx.send(author + ' has dealt '+ arg +' damage!')
-  await ctx.send(str(enemy.health) + '/' + str(enemy.maxHealth))
- 
-  if (enemy.health <= 0 and enemy.alive == 1):
-    await ctx.send(enemy.name + ' has been defeated! Great job warriors :crossed_swords:')
-    gameRunning = 0
-    enemy.alive = 0
-    for x in range(len(playerList)):
-      await ctx.send(playerList[x].name + ' has dealt '+ str(playerList[x].damageDealt) +' damage in total!')     
-      await ctx.send('The next enemy will be the boss monster "GIANT CRAB"')
+   playerList[UserNumThatSentMsg].damageDealt =  playerList[UserNumThatSentMsg].damageDealt + int(arg)    
+   enemy.health = enemy.health - int(arg)
+   await ctx.send(author + ' has dealt '+ arg +' damage!')
+   await ctx.send(str(enemy.health) + '/' + str(enemy.maxHealth))
+    
+   if (enemy.health <= 0 and enemy.alive == 1):
+     await ctx.send(enemy.name + ' has been defeated! Great job warriors :crossed_swords:')
+     enemy.alive = 0
+     for x in range(len(playerList)):
+       await ctx.send(playerList[x].name + ' has dealt '+ str(playerList[x].damageDealt) +' damage in total!')     
+    
+     await ctx.send('The next enemy will be the boss monster "GIANT CRAB"')
 
+        
+  else:
+    await ctx.send("Invalid rep range for "+ str(excercise[ExNum].name)  +" it is:" + str(excercise[ExNum].minRep) + " to " + str(excercise[ExNum].maxRep))
+    await ctx.send()
 
       
-      db.insert({'name': playerList[x].name, 'damage': playerList[x].damageDealt})
-      db.search(User.name == playerList[x].name)
+     # db.insert({'name': playerList[x].name, 'damage': playerList[x].damageDealt})
+     # db.search(User.name == playerList[x].name)
 
         
 
@@ -123,8 +176,14 @@ async def score(ctx):
 @bot.command(name='bg')
 async def bg(ctx):
   await ctx.send(playerList[-1].name)
- # await ctx.send(playerList[UserNumThatSentMsg].name)
+  # await ctx.send(playerList[UserNumThatSentMsg].name)
   await ctx.send(len(playerList))    
 
+@bot.command(name='restart')
+async def restart(ctx):
+  global Exnum
+  enemy.clear()
+  playerList.clear()
+  ExNum=null
 
-bot.run(os.getenv("TOKEN"))
+bot.run(os.getenv("TOKEN")) 
